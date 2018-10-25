f283a9efefa928e24ef333c963b2a5fdf46af452
Fergus MacConnell <fergus.macconnell@widerfunnel.com>
2018-10-25 16:12:21 -0700

    Add router configuration

--- src/client/client.js
+++ src/client/client.js
@@ -1,6 +1,12 @@
 // Entry Point for client side code base
 import React from 'react';
 import ReactDom from 'react-dom';
-import Home from './components/Home';
+import { BrowswerRouter } from 'react-router-dom';
+import Routes from './Routes';
 
-ReactDom.hydrate(<Home />, document.querySelector('#root'));
+ReactDom.hydrate(
+	<BrowswerRouter>
+		<Routes />
+	</BrowswerRouter>,
+	document.querySelector('#root')
+);
